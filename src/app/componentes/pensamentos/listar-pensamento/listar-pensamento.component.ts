8c180f2a150e72c499de36b6a77ff8f8bfac668a
Jefferson Rodrigues <jeffersonrodrigues5030@gmail.com>
2023-07-22 10:15:17 -0300

    Utilização de diretivas de componente, estruturais e expressões para estilizar elementos como ngClass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
 
+  listaPensamentos = [
+    {
+      id: '1',
+      conteudo: 'Aprendendo Angular',
+      autoria: 'Jeff',
+      modelo: 'modelo1'
+    },
+    {
+      id: '2',
+      conteudo: 'Aprendendo .NET, Aprendendo Java, Aprendendo Angular,Aprendendo .NET, Aprendendo Java, Aprendendo AngularAprendendo .NET, Aprendendo Java, Aprendendo AngularAprendendo .NET, Aprendendo Java, Aprendendo AngularAprendendo .NET, Aprendendo Java, Aprendendo AngularAprendendo .NET, Aprendendo Java, Aprendendo Angular,',
+      autoria: 'Jeff',
+      modelo: 'modelo2'
+    },
+    {
+      id: '3',
+      conteudo: 'Aprendendo Java',
+      autoria: 'Jeff',
+      modelo: 'modelo3'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
